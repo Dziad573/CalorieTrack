39f811ef24933dc5dea24d5045cae8ce42a4707c
Dziad573 <krystiansmagacz05@gmail.com>
2024-03-28 21:12:25 +0100

    Update

--- CalorieTrack/src/components/Counter/MainCounter.jsx
+++ CalorieTrack/src/components/Counter/MainCounter.jsx
@@ -1,19 +1,44 @@
 import { useState } from 'react'
 import styles from './MainCounter.module.css'
+import weightIcon from '../../assets/weight.png'
+import heightIcon from '../../assets/ruler.png'
+import dateIcon from '../../assets/calendar.png'
+import foodIcon from '../../assets/diet.png'
+import trainingIcon from '../../assets/runner.png'
+
 
 export function MainCounter({ style }) {
     const [weight, setWeight] = useState("");
     const [height, setHeight] = useState("");
     const [age, setAge] = useState("");
     const [gender, setGender] = useState("");
+    const [goal, setGoal] = useState("=");
+    
 
-    const calculateMaxCalories = () => {
+    const calculateMaxCalories = (includeTraining) => {
         const ppmmen = 10 * weight + 6.25 * height + 5 * age + 5;
         const ppmwomen = 10 * weight + 6.25 * height + 5 * age - 161;
-        if(gender === "men"){
-            return ppmmen;
-        }else if(gender === "women"){
-            return ppmwomen;
+        let maxCalories = 0;
+        let text = "Twoje dzienne zapotrzebowanie to: ";
+
+        if (gender === "men") {
+            maxCalories = ppmmen;
+        } else if (gender === "women") {
+            maxCalories = ppmwomen;
+        }
+
+        if (includeTraining) {
+            maxCalories *= 1.2;
+        }
+        if (includeTraining) {
+            text = "Twoje dzienne zapotrzebowanie wraz z treningiem to: ";
+        }
+        if (goal === "=") {
+            return text + Math.round(maxCalories) + " kcal";
+        } else if (goal === "-") {
+            return text + Math.round(maxCalories * 0.8) + " kcal";
+        } else if (goal === "+") {
+            return text + Math.round(maxCalories * 1.2) + " kcal";
         }
     };
 
@@ -42,10 +67,8 @@ export function MainCounter({ style }) {
         setGender(selectedGender);
     }
 
-    let maxDailyCalories = null;
-    
-    if(weight && height && age){
-        maxDailyCalories = calculateMaxCalories();
+    const handleGoalChange = (selectedGoal) => {
+        setGoal(selectedGoal);
     }
     
 
@@ -53,31 +76,66 @@ export function MainCounter({ style }) {
         <div>
             <section className={styles.counter} style={style} >
                 <div className={styles.inputs}>
-                    <input type="text" placeholder="Waga" value={weight} onChange={handleWeightChange} />
-                    <input type="text" placeholder="Wzrost" value={height} onChange={handleHeightChange} />
-                    <input type="text" placeholder="Wiek" value={age} onChange={handleAgeChange} />
+                    <div>
+                        <img src={weightIcon} className={styles.icon} alt="Weight Icon" />
+                        <input type="text" placeholder="Waga" value={weight} onChange={handleWeightChange} />
+                    </div>
+                    <div>
+                        <img src={heightIcon} className={styles.icon} alt="Height Icon" />
+                        <input type="text" placeholder="Wzrost" value={height} onChange={handleHeightChange} />
+                    </div>
+                    <div>
+                        <img src={dateIcon} className={styles.icon} alt="Date Age Icon" />
+                        <input type="text" placeholder="Wiek" value={age} onChange={handleAgeChange} />
+                    </div>
+
                 </div>
+                
                 <div className={styles.inputs}>
-                    
                     <button 
                         className={gender === "men" ? styles.selectedButton : styles.button} 
                         onClick={() => handleGenderChange("men")}
                     >
                         Mężczyzna
                     </button>
-                    
                     <button 
                         className={gender === "women" ? styles.selectedButton : styles.button} 
                         onClick={() => handleGenderChange("women")}
                     >
                         Kobieta
                     </button>
-                    
                 </div>
+
+                <div className={styles.inputs}>
+                    <select value={goal} onChange={(e) => handleGoalChange(e.target.value)}>
+                        <option value="=" defaultValue>1. Chcę utrzymać wagę</option>
+                        <option value="-">2. Chcę schudnąć</option>
+                        <option value="+">3. Chcę przytyć</option>
+                    </select>
+                    {/* <button onClick={() => handleCaltulate()}>
+                        Oblicz
+                    </button> */}
+                </div>
+                
                 <div className={styles.infoContainer}>
                     <div className={styles.infoDaily}>
                         <div>
-                            {maxDailyCalories}
+                            {weight && height && age && gender ? (
+                                <>
+                                    <div>
+                                        <img src={foodIcon} className={styles.icon} alt="Food Icon" />
+                                        {calculateMaxCalories(false)}
+                                    </div>
+                                    <div>
+                                        <img src={trainingIcon} className={styles.icon} alt="Training Icon" />
+                                        {calculateMaxCalories(true)}
+                                    </div>
+                                </>
+                            ) : (
+                                <div>
+                                    <p>Wprowadź wszystkie dane</p>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
